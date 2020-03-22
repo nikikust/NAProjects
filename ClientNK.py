faa51315d23508f03cc659a4b80a7ceef88a2f22
nikikust <lis291@yandex.ru>
2020-03-22 17:14:22 +0300

    Something

--- ClientNK.py
+++ ClientNK.py
@@ -5,11 +5,17 @@ import socket
 
 sock = socket.socket()
 sock.connect(('ff64.ddns.net', 5000))
+
 print('connected')
+
 data = sock.recv(1024)
 sock.close()
 
 print(data)
+
 print('done')
+
+'''
 while 1:
     continue
+'''
